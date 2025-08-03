1e6dcf07836c577207227fb4ffaf76e3e3429720
Ricketao <oniaronn@gmail.com>
2025-08-03 23:18:18 +0900

    ユニット5完了

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,11 @@
 import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 
+import preact from '@astrojs/preact';
+
 export default defineConfig({
   output: 'server',
   adapter: cloudflare(),
+  site: "https://example.com",
+  integrations: [preact()]
 });
